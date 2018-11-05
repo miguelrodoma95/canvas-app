31d825617cd6762a9c9a7512e749333e032ca6d8
Miguel Rodriguez <miguelrodoma95@gmail.com>
2018-11-04 23:50:59 -0600

    Add sqlite demo

--- app/src/main/java/com/canvas/on/canvascreator/Fragments/BusinessModelCanvasFragment.kt
+++ app/src/main/java/com/canvas/on/canvascreator/Fragments/BusinessModelCanvasFragment.kt
@@ -1,9 +1,13 @@
 package com.canvas.on.canvascreator.Fragments
 
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context.MODE_PRIVATE
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -35,5 +39,18 @@ class BusinessModelCanvasFragment : Fragment() {
 
         tvProjectName = activity!!.findViewById(R.id.tv_projectName)
         tvProjectName.text = canvasVM.projectName
+
+        var sqlDataBase: SQLiteDatabase = activity!!.openOrCreateDatabase("Canvas", MODE_PRIVATE, null)
+        sqlDataBase.execSQL("CREATE TABLE IF NOT EXISTS usercanvas (valueProp VARCHAR, customerSeg VARCHAR," +
+                "customerRelation VARCHAR, channels VARCHAR, keyAct VARCHAR, keyRes VARCHAR, keyPartners VARCHAR," +
+                "costStruct VARCHAR, revenueStream VARCHAR)")
+
+        sqlDataBase.execSQL("INSERT INTO usercanvas (valueProp) VALUES ('hola')")
+
+        var c: Cursor = sqlDataBase.rawQuery("SELECT valueProp FROM usercanvas", null)
+        c.moveToFirst()
+        var vpIndex: Int = c.getColumnIndex("valueProp")
+
+            Log.d("test", c.getString(0))
     }
 }
